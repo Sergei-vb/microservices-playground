12253ef92188ee8295740bd07db7b01161524945
Sergei Bogolepov <bogolepovsergei1@gmail.com>
2021-04-23 01:38:42 +0300

    add service and ingress entities

--- app/router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-
-router = APIRouter()
-
-
-@router.get('/health/')
-async def get_health():
-    return {'status': 'OK'}
--- server.py
+++ server.py
@@ -1,5 +1,21 @@
+import os
+
+from fastapi import FastAPI
 import uvicorn
 
 
+app = FastAPI(title='microservices-playground')
+
+
+@app.get('/health/')
+async def get_health():
+    return {'status': 'OK'}
+
+
+@app.get('/')
+async def get_root():
+    return 'Hello world from ' + os.environ['HOSTNAME'] + ' !'
+
+
 if __name__ == '__main__':
-    uvicorn.run('app.application:app', host='0.0.0.0', port=80, reload=False)
+    uvicorn.run('server:app', host='0.0.0.0', port=80, reload=False)
